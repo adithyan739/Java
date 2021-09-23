f6b05b2172003c50931bd5f95d4d173b10941b8d
adithyan739 <30692646+adithyan739@users.noreply.github.com>
2021-09-23 18:51:41 +0530

    Add files via upload

--- First.java
+++ First.java
@@ -1,9 +1,19 @@
 import java.applet.Applet;  
-import java.awt.Graphics;  
+import java.awt.*;  
+ 
 public class First extends Applet{  
-  
+ 
 public void paint(Graphics g){  
-g.drawString("welcome",150,150);  
+g.setColor(Color.red);  
+g.drawString("ghhh",80,70);
+g.drawLine(20,30,20,300);  
+g.drawRect(70,100,30,30);  
+g.fillRect(170,100,30,30);  
+g.drawOval(70,200,30,30);  
+ 
+g.setColor(Color.pink);  
+g.fillOval(170,200,30,30);  
+
+ 
 }  
-  
 }  
